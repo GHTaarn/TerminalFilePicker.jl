6854556aeb2edcb09b5d0a90cfac57d4045268e1
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2024-05-26 07:23:50 +0200

    New keyword: join
    Breaks backwards compatibility

--- src/TerminalFilePicker.jl
+++ src/TerminalFilePicker.jl
@@ -5,15 +5,21 @@ using REPL.TerminalMenus
 export pick_file
 
 """
-    pick_file(dir="."; pattern=r"")
+    pick_file(dir="."; pattern=r"", join=true)
 
 Show the files in `dir` that match `pattern` and return the one chosen by the
 user. Immediately returns `nothing` if there are no files to choose from.
+
+The `join` keyword works like in the Julia `readdir` function, i.e. if set to
+`false` then the raw filename is returned, if set to `true` then `pick_file`
+returns`joinpath(dir, fname)` where `fname` is the filename selected by the
+user.
 """
-function pick_file(dir="."; pattern=r"")
+function pick_file(dir="."; pattern=r"", join=true)
     files=filter(contains(pattern), readdir(dir))
     length(files) == 0 && return nothing
-    files[request(RadioMenu(files))]
+    fname = files[request(RadioMenu(files))]
+    join ? joinpath(dir, fname) : fname
 end
 
 end # module TerminalFilePicker
